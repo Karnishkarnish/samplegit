e53cb174fd89b7229102ea938f38a3737cc3acfb
vishnu1002 <vishnuramguru@gmail.com>
2025-07-29 10:23:40 +0000

    updated index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 age = input('Enter a Name:')
 if int(age) >= 18:
-    print("You are eligible to vote.")
+    print("You can vote.")
 else :
-    print("You are Not eligible to Vote.")
+    print("You cannot vote.")
